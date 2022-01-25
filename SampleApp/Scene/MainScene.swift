53bc52e672dfe0fb68ad2e48249cb84294eeae25
김낙진 <nakjin@mail.shinhan.dev>
2022-01-25 21:22:51 +0900

    Apply SwiftUI

---
+++ SampleApp/Scene/MainScene.swift
@@ -0,0 +1,20 @@
+//
+//  MainScene.swift
+//  SampleApp
+//
+//  Created by 60067670 on 2022/01/25.
+//
+
+import SwiftUI
+
+struct MainScene: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainScene_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScene()
+    }
+}
